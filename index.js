c5192fa4d992b932e0b850583f688e5f3b4db8e6
Ryan Dong <ryandong@Ryans-MacBook-Pro-2.local>
2023-01-20 19:45:14 -0800

    added index.js prompt

--- index.js
+++ index.js
@@ -22,3 +22,164 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 const questions = [];
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
+
+
+const generateHTML = require("./src/generateHTML");
+
+const team = [];
+
+function init() {
+    console.log("Welcome to the Team Profile Generator!");
+    console.log("Please build your team");
+    addManager();
+}
+
+const inquirer = require('inquirer');
+
+// team manager prompt
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter the team manager\'s name:',
+    },
+    {
+      type: 'input',
+      name: 'employeeId',
+      message: 'Enter the team manager\'s employee ID:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter the team manager\'s email address:',
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: 'Enter the team manager\'s office number:',
+    },
+  ])
+  .then((answers) => {
+    // use the answers to create the team manager object
+    const manager = {
+      name: answers.name,
+      employeeId: answers.employeeId,
+      email: answers.email,
+      officeNumber: answers.officeNumber,
+    };
+
+    // team member prompt
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'memberType',
+          message: 'What type of team member would you like to add?',
+          choices: ['Engineer', 'Intern', 'Finish Building Team'],
+        },
+      ])
+      .then((answers) => {
+        switch (answers.memberType) {
+          case 'Engineer':
+            // engineer prompt
+            inquirer
+              .prompt([
+                {
+                  type: 'input',
+                  name: 'name',
+                  message: 'Enter the engineer\'s name:',
+                },
+                {
+                  type: 'input',
+                  name: 'employeeId',
+                  message: 'Enter the engineer\'s employee ID:',
+                },
+                {
+                  type: 'input',
+                  name: 'email',
+                  message: 'Enter the engineer\'s email address:',
+                },
+                {
+                  type: 'input',
+                  name: 'githubUsername',
+                  message: 'Enter the engineer\'s GitHub username:',
+                },
+              ])
+              .then((answers) => {
+                // use the answers to create the engineer object
+                const engineer = {
+                  name: answers.name,
+                  employeeId: answers.employeeId,
+                  email: answers.email,
+                  githubUsername: answers.githubUsername,
+                };
+
+                // add the engineer to the team
+                // ...
+
+                // go back to the team member prompt
+                // ...
+              });
+            break;
+          case 'Intern':
+            // intern prompt
+            inquirer
+              .prompt([
+                {
+                  type: 'input',
+                  name: 'name',
+                  message: 'Enter the intern\'s name:',
+                },
+                {
+                  type: 'input',
+                  name: 'employeeId',
+                  message: 'Enter the intern\'s employee ID:',
+                },
+                {
+                  type: 'input',
+                  name: 'email',
+                  message: 'Enter the intern\'s email address:',
+                },
+                {
+                  type: 'input',
+                    name: 'school',
+                    message: 'Enter the intern\'s school:',
+                },
+                ])
+                .then((answers) => {
+                // use the answers to create the intern object
+                const intern = {
+                    name: answers.name,
+                    employeeId: answers.employeeId,
+                    email: answers.email,
+                    school: answers.school,
+                };
+
+                // add the intern to the team
+                // ...
+
+                // go back to the team member prompt
+                // ...
+                });
+            break;
+            case 'Finish Building Team':
+                // finish building team
+                // ...
+                break;
+        }
+        });
+    });
+
+
+    
+
+
+
+
+
